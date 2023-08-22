2d03efca5b492ba9c6e097087b4e523deedb6cdc
Jacquet12 <jacquetleme2021@gmail.com>
2023-08-22 17:32:11 -0300

    fix

--- Introducao/string avancado/script.js
+++ Introducao/string avancado/script.js
@@ -1,3 +1,6 @@
-console.log("O seu nome é :'Jacquet Leme' ");
-console.log("Meu nome é 'James' \nSou extrangeiro!");
-console.log(`A soma de 4 + 5 é = ${4 + 5 }`);
+// console.log("O seu nome é :'Jacquet Leme' ");
+// console.log("Meu nome é 'James' \nSou extrangeiro!");
+// console.log(`A soma de 4 + 5 é = ${4 + 5 }`);
+
+let idade = prompt("Digite a sua idade: ");
+console.log(`A sua idade é: ${idade}`);
